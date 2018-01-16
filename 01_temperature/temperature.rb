3e284d6592083c459e2b95f64daac8d2624b3918
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-01-16 20:26:34 +0100

    EXO_temp DONE

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
-#write your code here
+def ftoc(temp_en_f)
+    temp = (temp_en_f - 32.0)*(5.0/9.0)
+end
+
+
+def ctof(temp_en_c)
+  temp = (temp_en_c * (9.0 / 5.0)) + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,30 @@
-#write your code here
+def add(a,b)
+  a + b
+end
+
+def subtract(a,b)
+  a - b
+end
+
+
+def sum(array_nb)
+  somme = 0
+  array_nb.each { |nb| somme += nb }
+  somme
+end
+
+def multiply(array_nb_mult)
+  mult = 0
+  array_nb_mult.each do |nb_mult|
+    mult *= nb_mult
+    return mult
+  end
+end
+
+puts multiply(1,2,3)
+
+# def multiply(array_nb_mult)
+#   mult = 0
+#   array_nb_mult.each { |nb| mult *= nb }
+#   mult
+# end
