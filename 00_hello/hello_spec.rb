3184a113bff1c0f39b7aaf3c7b3313bf0c6903e1
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-01-16 16:53:10 +0100

    Exo1 DONE

--- 00_hello/hello_spec.rb
+++ 00_hello/hello_spec.rb
@@ -104,6 +104,7 @@ require "hello"
 describe "the hello function" do
   it "says hello" do
     expect(hello).to eq("Hello!")
+    # S'attend à ce que la fonction hello retourne la string "Hello!"
   end
 end
 
